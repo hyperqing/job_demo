dbad58f2adb825db3f0e37dc314edc0b18ef9ef2
HyperQing <469379004@qq.com>
2018-05-25 22:16:04 +0800

    成功使用Twig模板

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -9,7 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
  * Class HelloController
  * @package App\Controller
  */
-class HelloController
+class HelloController extends Controller
 {
     /**
      * @Route("/hello/number")
@@ -18,8 +19,12 @@ class HelloController
     {
         $number = mt_rand(0, 100);
 
+        return $this->render('hello/number.html.twig', [
+            'number' => $number,
+        ]);
+
         return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
+            '<html><body>Lucky number: ' . $number . '</body></html>'
         );
     }
 }
