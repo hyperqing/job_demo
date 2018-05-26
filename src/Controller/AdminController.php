06828a381fb81e71b11b6a6c647f7c54af576dcc
hyperqing <469379004@qq.com>
2018-05-26 22:07:09 +0800

    添加后台页面

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -6,17 +6,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * 首页控制器
+ * 后台控制器
  * @package App\Controller
  */
-class IndexController extends Controller
+class AdminController extends Controller
 {
     /**
-     * 首页
-     * @Route("/", name="_index")
+     * 后台首页
+     * @Route("/admin",name="_admin")
      */
     public function index()
     {
-        return $this->render('index/index.html.twig');
+        return $this->render('admin/index.html.twig');
     }
 }
