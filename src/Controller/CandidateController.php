d79150ea0b1562826900398538d8dc32cd2aae78
hyperqing <469379004@qq.com>
2018-05-26 23:06:10 +0800

    实现表单提交

--- src/Controller/CandidateController.php
+++ src/Controller/CandidateController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -15,11 +16,13 @@ class CandidateController extends Controller
      * 添加新候选人
      * @Route("/candidate/save",name="_candidate_save")
      */
-    public function save()
+    public function save(Request $request)
     {
+        $form_data = $request->request->all();
         return $this->json([
             'status' => 1,
             'info' => '添加成功',
+            'data' => $form_data,
         ]);
     }
 }
